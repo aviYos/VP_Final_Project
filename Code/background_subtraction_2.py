9fbec7fceaf13dac50537e944cc51c5c1232deed
aviYos <63791779+aviYos@users.noreply.github.com>
2022-05-24 16:37:04 +0300

    Updates

--- Code/background_subtraction_2.py
+++ Code/background_subtraction_2.py
@@ -24,19 +24,8 @@ class background_subtractor:
         self.fps = project_utils.get_video_fps(self.video_cap)
         self.median_kernel_dense = project_constants.MEDIAN_FILTER_DENSE
         self.median_kernel_radius = (self.median_kernel_dense - 1) // 2
-
-    def initialize_median_filter_parameters(self):
-        try:
-            median_of_first_k_frames_S = np.median(
-                np.array(self.all_frames_Sat_channel_values[0:self.median_kernel_dense]),
-                axis=0)
-            median_of_last_k_frames_S = np.median(np.array(
-                self.all_frames_Sat_channel_values[
-                len(self.all_frames_Sat_channel_values) - self.median_kernel_dense:]),
-                axis=0)
-            return median_of_first_k_frames_S, median_of_last_k_frames_S
-        except Exception as e:
-            self.logger.error('Error in background subtraction: ' + str(e), exc_info=True)
+        self.bg_sub_masks = np.zeros((self.number_of_frames, self.frame_height, self.frame_width)).astype(np.uint8)
+        self.last_frame = None
 
     @staticmethod
     def get_bounding_rect_pixels(frame, bound_rect):
@@ -44,83 +33,34 @@ class background_subtractor:
         mask[bound_rect[1]:bound_rect[1] + bound_rect[3], bound_rect[0]:bound_rect[0] + bound_rect[2]] = 1
         return mask.astype(np.uint8)
 
-    def create_median_mask_for_frame(self, frame_index, median_of_first_k_frames_S, median_of_last_k_frames_S):
-        try:
-            if frame_index < self.median_kernel_radius:
-                median_mask = (self.all_frames_Sat_channel_values[frame_index, :,
-                               :] - median_of_first_k_frames_S) > project_constants.MEDIAN_FILTER_THRESHOLD
-
-            elif frame_index > (self.number_of_frames - self.median_kernel_radius - 1):
-                median_mask = (self.all_frames_Sat_channel_values[frame_index, :,
-                               :] - median_of_last_k_frames_S) > project_constants.MEDIAN_FILTER_THRESHOLD
-
-            else:
-                median_per_frame_num_S = np.median(
-                    np.array(self.all_frames_Sat_channel_values[frame_index, :, :][
-                             frame_index - self.median_kernel_radius:frame_index + self.median_kernel_radius + 1]),
-                    axis=0)
-                median_mask = ((self.all_frames_Sat_channel_values[frame_index, :,
-                                :] - median_per_frame_num_S) > project_constants.MEDIAN_FILTER_THRESHOLD)
-
-            median_mask = np.uint8(255. * median_mask)
-            median_mask = cv2.morphologyEx(median_mask, cv2.MORPH_DILATE,
-                                           cv2.getStructuringElement(cv2.MORPH_RECT, (3, 3)),
-                                           iterations=1)
-            # plt.imshow(median_mask, cmap='gray')
-            # plt.show()
-
-            median_mask, _, _ = self.get_largest_connected_shape_in_mask(
-                median_mask)
-
-            # plt.imshow(median_mask, cmap='gray')
-            # plt.show()
-
-            return median_mask
-        except Exception as e:
-            self.logger.error('Error in background subtraction: ' + str(e), exc_info=True)
-
     def create_knn_subtractor_mask_for_frame(self, foreground_mask):
         try:
-            _, knn_mask = cv2.threshold(foreground_mask, 0, 255, cv2.THRESH_OTSU)  # Binarize the BS frame
+            _, knn_mask = cv2.threshold(foreground_mask, 30, 255, cv2.THRESH_OTSU)  # Binarize the BS frame
 
             # plt.imshow(knn_mask, cmap='gray')
             # plt.show()
 
-            knn_mask = cv2.morphologyEx(knn_mask, cv2.MORPH_DILATE, cv2.getStructuringElement(cv2.MORPH_RECT, (9, 9)),
+            knn_mask = cv2.morphologyEx(knn_mask, cv2.MORPH_ERODE, cv2.getStructuringElement(cv2.MORPH_RECT, (3, 3)),
+                                        iterations=3)  # separate foreground from BG noises not filtered by KNN
+
+            knn_mask = cv2.morphologyEx(knn_mask, cv2.MORPH_DILATE,
+                                        cv2.getStructuringElement(cv2.MORPH_RECT, (2, 14)),
+                                        iterations=4)
+
+            knn_mask, _, _ = self.get_largest_connected_shape_in_mask(
+                knn_mask)  # Neutralize noises by eliminating all blobs other than the largest bloB
+
+            knn_mask = cv2.morphologyEx(knn_mask, cv2.MORPH_ERODE,
+                                        cv2.getStructuringElement(cv2.MORPH_RECT, (2, 5)),
                                         iterations=4)
 
             # plt.imshow(knn_mask, cmap='gray')
             # plt.show()
-
-            knn_mask = cv2.morphologyEx(knn_mask, cv2.MORPH_ERODE, cv2.getStructuringElement(cv2.MORPH_RECT, (5, 6)),
-                                        iterations=7)  # separate foreground from BG noises not filtered by KNN
-
-            # plt.imshow(knn_mask, cmap='gray')
-            # plt.show()
-
-            knn_mask, _, _ = self.get_largest_connected_shape_in_mask(
-                knn_mask)  # Neutralize noises by eliminating all blobs other than the largest blob
-
-            # plt.imshow(knn_mask, cmap='gray')
-            # plt.show()
-
-            # dilate to compensate for erode operations
             return knn_mask
         except Exception as e:
             self.logger.error('Error in background subtraction: ' + str(e), exc_info=True)
 
-    def create_skin_mask_for_frame(self, hue_channel, sat_channel, value_channel):
-        try:
-            # Detect skin using thresholding
-            skin_mask = (hue_channel < project_constants.SKIN_HUE_threshold) & (
-                    sat_channel > project_constants.SKIN_SAT_THRESHOLD_LOW) & (
-                                sat_channel < project_constants.SKIN_SAT_THRESHOLD_UPPER) & (
-                                value_channel < project_constants.SKIN_VALUE_threshold)
-            return np.uint8(255 * skin_mask)
-        except Exception as e:
-            self.logger.error('Error in background subtraction: ' + str(e), exc_info=True)
-
-    def union_masks(self, median_filter_mask, knn_mask, Hue, Value):
+    def union_masks(self, median_filter_mask, knn_mask, Value):
         try:
             # Unite all masks
             masks_union = median_filter_mask | knn_mask
@@ -164,6 +104,44 @@ class background_subtractor:
         except Exception as e:
             self.logger.error('Error in background subtraction: ' + str(e), exc_info=True)
 
+    def apply_grabcut(self, bound_rect, frame_hsv, union_mask):
+        try:
+
+            eroded_mask = cv2.morphologyEx(union_mask, cv2.MORPH_ERODE,
+                                           cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (5, 5)),
+                                           iterations=2)
+
+            dilated_mask = cv2.morphologyEx(union_mask, cv2.MORPH_DILATE,
+                                            cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (5, 5)),
+                                            iterations=2)
+
+            diff = dilated_mask - eroded_mask
+            _, diff = cv2.threshold(diff, 30, 255, cv2.THRESH_OTSU)  # Binarize the BS frame
+
+            diff2, _, _ = self.get_largest_connected_shape_in_mask(
+                diff)
+
+            mask = np.zeros(frame_hsv.shape[:2], np.uint8)
+            bgdModel = np.zeros((1, 65), np.float64)
+            fgdModel = np.zeros((1, 65), np.float64)
+            cv2.grabCut(frame_hsv, mask, bound_rect, bgdModel, fgdModel, 5, cv2.GC_INIT_WITH_RECT)
+            mask2 = np.where((mask == 2) | (mask == 0), 0, 1).astype('uint8')
+            frame_hsv = frame_hsv * mask2[:, :, np.newaxis]
+            plt.imshow(frame_hsv), plt.colorbar(), plt.show()
+        except Exception as e:
+            self.logger.error('Error in background subtraction: ' + str(e), exc_info=True)
+
+    def find_largest_contour(self, frame, fill=False):
+
+        frame = frame.astype(np.uint8)
+        contours, hierarchy = cv2.findContours(
+            frame,
+            cv2.RETR_TREE,
+            cv2.CHAIN_APPROX_SIMPLE
+        )
+        largest_contour = max(contours, key=cv2.contourArea)
+        return largest_contour
+
     def get_largest_connected_shape_in_mask(self, mask):
         try:
             number_of_labels, component_labels, stats, centroids = cv2.connectedComponentsWithStats(mask)
@@ -182,45 +160,58 @@ class background_subtractor:
         except Exception as e:
             self.logger.error('Error in background subtraction: ' + str(e), exc_info=True)
 
-    """def largest_component_mask(self, bin_img):
-        Finds the largest component in a binary image and returns the component as a mask.
-
-        contours = cv2.findContours(bin_img, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)[0]
-        # should be [1] if OpenCV 3+
-
-        max_area = 0
-        max_contour_index = 0
-        for i, contour in enumerate(contours):
-            contour_area = cv2.moments(contour)['m00']
-            if contour_area > max_area:
-                max_area = contour_area
-                max_contour_index = i
-
-        labeled_img = np.zeros(bin_img.shape, dtype=np.uint8)
-        cv2.drawContours(labeled_img, contours, max_contour_index, color=255, thickness=-1)
-
-        return labeled_img"""
-
     def train_background_subtractor_knn(self):
+        T = 10
+        all_hsv_frames = []
         try:
             self.logger.debug(' training knn subtractor on  stabilized video')
 
             self.all_frames_Sat_channel_values = \
                 np.zeros((self.number_of_frames, int(self.frame_height), int(self.frame_width)))
 
-            for frame_index in range(self.number_of_frames):
-                _, frame = self.video_cap.read()
-                if frame is None:
-                    break
+            self.bg_sub_masks = np.zeros((self.number_of_frames, self.frame_height, self.frame_width)).astype(np.uint8)
 
-                frame_hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-                _, sat_channel, _ = cv2.split(frame_hsv)
-                _ = self.knn_subtractor.apply(sat_channel)
-                self.all_frames_Sat_channel_values[frame_index, :, :] = sat_channel
-            self.video_cap.set(cv2.CAP_PROP_POS_FRAMES, 0)
+            for i in range(T):
+                for frame_index in range(self.number_of_frames):
+                    _, frame = self.video_cap.read()
+                    if frame is None:
+                        break
+                    frame_hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+                    if not i:
+                        all_hsv_frames.append(frame_hsv)
 
+                    _, sat_channel, value_channel = cv2.split(frame_hsv)
+
+                    self.all_frames_Sat_channel_values[frame_index, :, :] = sat_channel
+                    self.bg_sub_masks[frame_index, :, :] = self.knn_subtractor.apply(sat_channel)
+
+                self.video_cap.set(cv2.CAP_PROP_POS_FRAMES, 0)
+                hsv_median_frame = np.mean(all_hsv_frames, axis=0).astype(dtype=np.uint8)
+                return hsv_median_frame
         except Exception as e:
             self.logger.error('Error in background subtraction: ' + str(e), exc_info=True)
+
+    def try_k_means(self, frame_hsv, rect):
+        frame = cv2.cvtColor(frame_hsv, cv2.COLOR_HSV2BGR)
+        frame = project_utils.slice_frame_from_bounding_rect(frame, rect)
+        twoDimage = frame.reshape((-1, 3))
+        twoDimage = np.float32(twoDimage)
+        criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 10, 1.0)
+        K = 2
+        attempts = 10
+        ret, label, center = cv2.kmeans(twoDimage, K, None, criteria, attempts, cv2.KMEANS_PP_CENTERS)
+        center = np.uint8(center)
+        res = center[label.flatten()]
+        result_image = res.reshape((frame.shape))
+        plt.imshow(result_image)
+
+    def get_image_derivative(self, frame):
+        edges = cv2.Canny(frame, 100, 200)
+        eroded_mask = cv2.morphologyEx(edges, cv2.MORPH_CLOSE,
+                                       cv2.getStructuringElement(cv2.MORPH_RECT, (7, 7)),
+                                       iterations=2)
+        plt.imshow(eroded_mask, cmap='gray')
+        self.find_largest_contour(eroded_mask)
 
     def color_filter(self, frame_hsv, rect, bounding_box_mask, union_masks):
 
@@ -229,6 +220,11 @@ class background_subtractor:
         sliced_sat = project_utils.slice_frame_from_bounding_rect(Sat, rect)
         sliced_hue = project_utils.slice_frame_from_bounding_rect(Hue, rect)
         sliced_value = project_utils.slice_frame_from_bounding_rect(Value, rect)
+
+        sliced_hsv_image = project_utils.slice_frame_from_bounding_rect(frame_hsv, rect)
+
+        # self.get_image_derivative(sliced_hsv_image)
+        # self.try_k_means(frame_hsv, rect)
 
         median_of_sat = np.median(sliced_sat)
         _, out_sat = cv2.threshold(sliced_sat, median_of_sat, 255, cv2.THRESH_BINARY)
@@ -261,6 +257,36 @@ class background_subtractor:
                                                                                      final_mask)
         return original_size_final_mask.astype(np.uint8)
 
+    def new_median_filter(self, median_frame_hsv, hsv):
+        dframe = cv2.absdiff(hsv, median_frame_hsv)
+        _, dframe_sat = cv2.threshold(dframe[:,:,1], 10, 255, cv2.THRESH_BINARY)
+
+        dframe_sat,_,_ = self.get_largest_connected_shape_in_mask(dframe_sat)
+
+        _, dframe_val = cv2.threshold(dframe[:, :, 2], 10, 255, cv2.THRESH_BINARY)
+
+        dframe_val,_,_ = self.get_largest_connected_shape_in_mask(dframe_val)
+
+        dframe_sat = cv2.morphologyEx(dframe_sat, cv2.MORPH_DILATE,
+                                       cv2.getStructuringElement(cv2.MORPH_RECT, (5, 5)),
+                                       iterations=2)
+
+        dframe_val = cv2.morphologyEx(dframe_val, cv2.MORPH_DILATE,
+                                      cv2.getStructuringElement(cv2.MORPH_RECT, (5, 5)),
+                                      iterations=2)
+
+        median_mask = dframe_sat & dframe_val
+
+        final_median_mask = cv2.morphologyEx(median_mask, cv2.MORPH_CLOSE,
+                                      cv2.getStructuringElement(cv2.MORPH_RECT, (30, 30)),
+                                      iterations=2)
+
+        final_median_mask,_,_ = self.get_largest_connected_shape_in_mask(final_median_mask)
+
+        plt.imshow(final_median_mask)
+
+        return final_median_mask
+
     def main_background_subtraction_module(self):
 
         self.logger.debug('running background subtraction on stabilized video')
@@ -279,11 +305,7 @@ class background_subtractor:
 
         try:
 
-            self.train_background_subtractor_knn()
-
-            self.logger.debug('fetching data for median filter')
-
-            median_of_first_k_frames_S, median_of_last_k_frames_S = self.initialize_median_filter_parameters()
+            median_frame_hsv = self.train_background_subtractor_knn()
 
             self.logger.debug('looping over all video frames and running background subtraction')
 
@@ -304,35 +326,38 @@ class background_subtractor:
 
                 Hue, Sat, Value = cv2.split(frame_hsv)
 
-                self.logger.debug('Creating median mask for frame number ' + str(frame_index))
+                median_filter_mask = self.new_median_filter(median_frame_hsv, frame_hsv)
 
-                median_filter_mask = self.create_median_mask_for_frame(frame_index, median_of_first_k_frames_S,
-                                                                       median_of_last_k_frames_S)
+                self.logger.debug('Creating median mask for frame number ' + str(frame_index))
 
                 self.logger.debug('Creating foreground mask for frame number ' + str(frame_index))
 
-                foreground_mask_knn = self.knn_subtractor.apply(Sat)
+                foreground_mask_knn = self.bg_sub_masks[frame_index, :, :]
                 knn_mask = self.create_knn_subtractor_mask_for_frame(foreground_mask_knn)
 
                 self.logger.debug('Creating skin mask for frame number ' + str(frame_index))
 
                 self.logger.debug('Creating union mask from all masks for frame number ' + str(frame_index))
 
-                union_masks = self.union_masks(median_filter_mask, knn_mask, Hue, Value)
+                final_mask = self.union_masks(median_filter_mask, knn_mask, Value)
 
                 self.logger.debug('writing  frame number ' + str(frame_index) + ' to binary video')
 
-                bound_rect = cv2.boundingRect(union_masks)
+                bound_rect = cv2.boundingRect(final_mask)
 
                 bounding_box_mask = self.get_bounding_rect_pixels(frame, bound_rect)
 
-                final_mask = self.color_filter(frame_hsv, bound_rect, bounding_box_mask, union_masks)
+                # final_mask = self.apply_grabcut(bound_rect,frame, union_masks)
+
+                #final_mask = self.color_filter(frame_hsv, bound_rect, bounding_box_mask, union_masks)
 
                 vid_writer_binary.write(final_mask)
 
                 self.logger.debug('writing  frame number ' + str(frame_index) + ' to extracted video')
 
                 vid_writer_extracted.write(cv2.bitwise_and(frame, frame, mask=final_mask.astype(np.uint8)))
+
+                self.last_frame = frame_hsv
 
             self.logger.debug('saving binary video')
 
